6166ea95ffef031441878c073863cf5da99c11df
Lucas <lucasdecassia9@hotmail.com>
2024-07-30 15:11:56 -0300

    implemetation clock

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-  document.title = `Você clicou ${count} vezes`;
-   },[count]);
+  const [hora, setHora] = useState(4);
+  const [minuto, setMinuto] = useState(30);
+  const [segundos, setSegundos] = useState(1);
+
+  function callAlert(){
+
+  }
+
+  useEffect(() =>{
+
+    const interval = setInterval(() => {
+      
+     setSegundos(segundos+1);
+      if(segundos == 59){
+        setSegundos(0);
+        setMinuto(minuto+1);
+        if(minuto == 59){
+          setMinuto(0);
+          setHora(hora+1);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+
+  });
 
 
   return (
     <div>
-      <p>Você clicou {count} vezes</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click aqui
-      </button>
+      <h1 style={{textAlign: 'center'}}>{hora}:{minuto}:{segundos}</h1>
     </div>
   );
 }
